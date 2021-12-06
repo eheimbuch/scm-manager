86028d86fefa4fb6d686b78304d62260a6d59ca1
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-12-06 11:46:00 +0100

    Introduce re-index repository event

---
+++ scm-webapp/src/main/java/sonia/scm/importexport/ReindexOnImportTrigger.java
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.importexport;
+
+import com.github.legman.Subscribe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sonia.scm.EagerSingleton;
+import sonia.scm.event.ScmEventBus;
+import sonia.scm.plugin.Extension;
+import sonia.scm.repository.RepositoryImportEvent;
+import sonia.scm.search.ReindexRepositoryEvent;
+
+import javax.inject.Inject;
+
+@EagerSingleton
+@Extension
+public class ReindexOnImportTrigger {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ReindexOnImportTrigger.class);
+
+  private final ScmEventBus eventBus;
+
+  @Inject
+  public ReindexOnImportTrigger(ScmEventBus eventBus) {
+    this.eventBus = eventBus;
+  }
+
+  @Subscribe
+  public void event(RepositoryImportEvent event) {
+    if (!event.isFailed()) {
+      LOG.info("triggering re-index for repository {}", event.getItem());
+      eventBus.post(new ReindexRepositoryEvent(event.getItem()));
+    }
+  }
+}
